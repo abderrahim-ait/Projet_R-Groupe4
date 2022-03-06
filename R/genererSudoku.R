0d2f6fc0968f2d19c6bcc01dc52eafec53bf8f5c
Jiayue YUAN <91314965+JiayueY@users.noreply.github.com>
2022-03-06 23:31:10 +0100

    Update and rename genererSoduko.R to genererSudoku.R

--- R/genererSudoku.R
+++ R/genererSudoku.R
@@ -1,17 +1,17 @@
 
-#' Suprimer des valeur aléatoire dans un Soduku
+#' Suprimer des valeur aléatoire dans un Sodoku
 #'
 #'
 #'@param A : une matrice
 #'@param N ; nombre de valeur à suprimer
 #'@return une matrice de tail 9x9 qui contient les valeurs vide
-#'@author AIT MOULAY ABDERRAHIM
+#'@author AIT MOULAY ABDERRAHIM, JIAYUE YUAN
 #'@examples
-#' A = genererSoduko (matrice , nombre de valeur )
+#' A = genererSudoku (matrice , nombre de valeur )
 #'@export
 #'
 
-genererSoduko <- function (A,N) {
+genererSudoku <- function (A,N) {
 
   k= sample(1:9,N,replace = TRUE)
   j= sample(1:9,N,replace = TRUE)
