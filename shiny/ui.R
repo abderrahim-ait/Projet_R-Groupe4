2bca0ac518f4f996925eeeb0b642f81319f44f12
Jiayue YUAN <91314965+JiayueY@users.noreply.github.com>
2022-03-11 19:38:01 +0100

    Update ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,37 +1,72 @@
 library(shiny)
 
 fluidPage(
-          sidebarLayout(
-            sidebarPanel(
-              titlePanel("Sudoku Game"),
-              helpText(a('Github', href='https://github.com/abderrahim-ait/Projet_R-Groupe4',target="_blank"))
-              hr(),
-              sliderInput("Niveau", label = "selectionnez le coefficent de difficulte",
-                          min = 1, max = 4, value = 1),
-              actionButton("newButton", "Nouveau Sudoku"),
-              hr(),
-              h4('Solve'),
-              actionButton("solveButton", "Solution"),
-
-              selectInput("row", label = "Row",
-                          choices = list(1,2,3,4,5,6,7,8,9),
-                          selected = 1),
-              selectInput("col", label = "Column",
-                          choices = list(1,2,3,4,5,6,7,8,9),
-                          selected = 1),
-              selectInput("value", label = "Value",
-                          choices = list(1,2,3,4,5,6,7,8,9," "=0),
-                          selected = 0),
-              actionButton("setButton", "Set"),
-              hr()
+  titlePanel(
+            titlePanel("Sudoku Game")
             ),
-            mainPanel(
-              tabsetPanel(
-                tabPanel("Play", plotOutput("table")),
-                tabPanel("Regle du jeu", includeHTML("regle.html")),
-              )
-            )
-          )
-)
+  helpText(
+            a(
+            'Notre Github site',
+             href='https://github.com/abderrahim-ait/Projet_R-Groupe4.git')
+          ),
+  sidebarLayout(
+    sidebarPanel (
+      selectInput(
+                inputId = "diff",label ="Selectionner le niveau:",
+                  choices = c("Facile","Moyenne","Difficile","Expert")
+      ),
+
+      column(6,
+             actionButton("Nouveau" ,label= "Nouveau Sudoku")),
+      hr(),
+      br(),
+              
+      fluidRow(
+        column(
+          selectInput(
+                    inputId = "ligne",
+                    label ="Selectionnez la linge",
+                    choices = list(1,2,3,4,5,6,7,8,9),
+                    selected = 1),
+          width = 5,
+          br(),
+                  
+          selectInput(inputId = "colonne",
+                      label = "Sélectionnez la colonne",
+                      choices = list(1,2,3,4,5,6,7,8,9),
+                      selected = 1),
 
 
+          br(),
+          selectInput(inputId = "valeur",
+                      label = "Sélectionnez la valeur",
+                      choices = list(1,2,3,4,5,6,7,8,9)),
+
+          br(),
+          actionButton(inputId = "jouer",
+                       label="Jouer"),
+                  
+          hr(),
+                  
+          h4('Solution automatique:'),
+          actionButton("Solution",
+                       label = "Afficher"),
+
+        ),
+
+      )),
+
+    mainPanel(
+      tabsetPanel(
+      tabPanel("Game",plotOutput("tableSudoku")),
+      tabPanel("Règle",HTML(text = "Une grille de Sudoku peut etre vue comme une matrice carr ́ee comprenant 9 lignes et 9 colonnes. 
+                                    Chacun des 81 elements de la matrice appartient à l’ensemble {1, 2, 3, . . . , 8, 9}. 
+                                    La matrice est divisee en 9 blocs (3 × 3) contenant chacun 9 elements (3 lignes et 3 colonnes). 
+                                    Les nombres de sont repartis suivant les trois regles suivantes :
+                                    1) chaque chiffre n’apparaˆıt qu’une fois en ligne, 
+                                    2) chaque chiffre n’apparait qu’une fois en colonne, 
+                                    3) chaque chiffre n’apparait qu’une fois dans un bloc.")),
+
+
+    ))
+))
